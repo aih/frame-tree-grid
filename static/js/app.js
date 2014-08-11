76787d23dbf03c99bbd8fbd8abd3afc6258d4930
aih <aih@tabulaw.com>
2014-08-10 22:01:36 -0700

    Use typeahead to select file name

--- static/js/app.js
+++ static/js/app.js
@@ -96,18 +96,6 @@ myApp.controller('typeaheadDemoController', ['$scope', function($scope){
  $scope.select2Options = {
         allowClear:true
     }; 
-  $scope.numbers =  [
-      { value: '1', text: 'one' },
-      { value: '2', text: 'two' },
-      { value: '3', text: 'three' },
-      { value: '4', text: 'four' },
-      { value: '5', text: 'five' },
-      { value: '6', text: 'six' },
-      { value: '7', text: 'seven' },
-      { value: '8', text: 'eight' },
-      { value: '9', text: 'nine' },
-      { value: '10', text: 'ten' }
-    ];
    
     }
 ]);
